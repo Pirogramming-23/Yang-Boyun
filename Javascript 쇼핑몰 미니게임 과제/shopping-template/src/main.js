69ea3c8678edb47e75c5f43fb0312bc9e54cf7e5
Yang-Boyun <boyunyang@naver.com>
2025-07-03 02:37:34 +0900

    JavaScript - display items(이전 오타 수정)

--- Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
@@ -6,11 +6,27 @@ function loadItems(){
     .then(json => json.itmes);
 }
 
+// Update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('items');
+    const html = items.map(item => createHTMLString(item)).join('');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item_thumbnail">
+        <span class="item__description">${item.gender},${item.size}</span>
+    </li>
+   ` ;
+}
+
 // main
 loadItems()
 .then(items => {
-    console.log(items);
-   //  displayItems(items);
-   //  setEventListener(items) 
+   displayItems(items);
+   setEventListener(items);
 })
 .catch(console.log)

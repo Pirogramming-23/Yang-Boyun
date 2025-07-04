b630363187aaafb4cbf37d84d9d164d40d5e783f
Yang-Boyun <boyunyang@naver.com>
2025-07-05 07:43:22 +0900

    5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -19,3 +19,19 @@ while True:
 for i in range(1, count + 1):
     num += 1
     print(f'playerA : {num}')
+
+#5단계
+# playerB 입력 및 출력
+while True:
+    try:
+        count_b = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+        if count_b not in [1, 2, 3]:
+            print('1,2,3 중 하나를 입력하세요')
+            continue
+        break
+    except ValueError:
+        print('정수를 입력하세요')
+
+for i in range(1, count_b + 1):
+    num += 1
+    print(f'playerB : {num}')

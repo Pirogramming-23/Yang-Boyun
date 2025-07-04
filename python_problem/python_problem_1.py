22dc7468bf1b915440d288c2f0e757b3652cc224
Yang-Boyun <boyunyang@naver.com>
2025-07-05 07:53:56 +0900

    6단계(이전단계 오류 수정)

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -16,7 +16,7 @@ while True:
         print('정수를 입력하세요')
 
 #4단계
-for i in range(1, count + 1):
+for i in range(count):
     num += 1
     print(f'playerA : {num}')
 
@@ -32,6 +32,27 @@ while True:
     except ValueError:
         print('정수를 입력하세요')
 
-for i in range(1, count_b + 1):
+for i in range(count):
     num += 1
     print(f'playerB : {num}')
+
+#6단계
+turn = 'playerA'
+while num < 31:
+    while True:
+        try:
+            count = int(input(f'부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
+            if count not in [1, 2, 3]:
+                print('1,2,3 중 하나를 입력하세요')
+                continue
+            break
+        except ValueError:
+            print('정수를 입력하세요')
+    for i in range(1, count + 1):
+        num += 1
+        print(f'{turn} : {num}')
+        if num == 31:
+            break
+    if num == 31:
+        break
+    turn = 'playerB' if turn == 'playerA' else 'playerA'
